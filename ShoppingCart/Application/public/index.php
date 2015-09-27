d5dfef62fd1ce56286904aee68521cfd679b1f9d
MartinKurshumov <kur6uma666@gmail.com>
2015-09-27 17:50:20 +0300

    Implemented includeConfigFile function and magic property

--- ShoppingCart/Application/public/index.php
+++ ShoppingCart/Application/public/index.php
@@ -1,8 +1,14 @@
 <?php
 
 use Framework\App;
+use Framework\Config;
 
 include '../../Framework/App.php';
 
 $app = App::getInstance();
+
+$config = Config::getInstance();
+$config->setConfigFolder('../config');
+echo $config->app['test1'];
+
 $app->run();
---
+++ ShoppingCart/Framework/Config.php
@@ -0,0 +1,65 @@
+<?php
+namespace Framework;
+
+
+class Config
+{
+    private static $_instance = null;
+    private $_configArray = array();
+    private $_configFolder = null;
+
+    private function __construct() {
+
+    }
+
+    public function setConfigFolder($configFolder) {
+        if (!$configFolder) {
+            throw new \Exception('Empty config folder path:');
+        }
+        $_configFolder = realpath($configFolder);
+        if ($_configFolder != false && is_dir($_configFolder) && is_readable($_configFolder)) {
+            //clear old config data
+            $this->_configArray = array();
+            $this->_configFolder = $_configFolder . DIRECTORY_SEPARATOR;
+        } else {
+            throw new \Exception('Config directory read error:' . $configFolder);
+        }
+    }
+
+    public function includeConfigFile($path) {
+        if (!$path) {
+            //TODO
+            throw new \Exception;
+        }
+        $_file = realpath($path);
+        if ($_file != false && is_file($_file) && is_readable($_file)) {
+            $_basename = explode('.php', basename($_file))[0];
+            include $_file;
+            $this->_configArray[$_basename] = $cnf;
+        } else {
+            //TODO
+            throw new \Exception('Config file read error:' . $path);
+        }
+    }
+
+    public function __get($name) {
+        if (!$this->_configArray[$name]) {
+            $this->includeConfigFile($this->_configFolder . $name . '.php');
+        }
+        if (array_key_exists($name, $this->_configArray)) {
+            return $this->_configArray[$name];
+        }
+        return null;
+    }
+
+    /**
+     * @return Config
+     */
+    public static function getInstance() {
+        if (self::$_instance == null) {
+            self::$_instance = new Config();
+        }
+
+        return self::$_instance;
+    }
+}
