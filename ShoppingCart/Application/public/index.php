266a63cf938adeb7dc2f15c71a67a59575dfb9a4
MartinKurshumov <kur6uma666@gmail.com>
2015-09-28 19:44:18 +0300

    Added native sessions with all of their functions

--- ShoppingCart/Application/public/index.php
+++ ShoppingCart/Application/public/index.php
@@ -2,12 +2,12 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 use Framework\App;
+use Framework\Database\SimpleDb;
+
 include '../../Framework/App.php';
 
 $app = App::getInstance();
+$db = new SimpleDb();
 
-$db = new \Framework\Database\SimpleDb();
-$a = $db->prepare('SELECT * FROM users')->execute()->fetchAllAssoc();
-print_r($a);
 
 $app->run();
